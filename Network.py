fb6e708f77f59e9bbd43d793c5200ab260e0a401
RosmaAp11 <119741238+RosmaAp11@users.noreply.github.com>
2022-12-05 16:40:20 +0700

    Update Network.py

--- Network.py
+++ Network.py
@@ -7,8 +7,6 @@ Network_model =  pickle.load(open('Network_model.sav', 'rb'))
 #Judul Web
 st.title('Data Mining Prediksi pelanggan mengklik iklan')
 
-User_id = st.text_input('Masukan id atau kode pelanggan')
-
 Gender = st.text_input('Jenis Kelamin')
 
 Age = st.text_input('Masukan usia konsumen')
@@ -21,7 +19,7 @@ Network_diagnosis = ''
 
 # membuat tombol untuk prediksi
 if st.button('Test Prediksi'):
-    Network_prediction = Network_model.predict([[User_id, Gender, Age, EstimatedSalary]])
+    Network_prediction = Network_model.predict([[Gender, Age, EstimatedSalary]])
     
     if(Network_prediction[0] == 1):
         Network_diagnosis = 'Pelanggan Mengklik Iklan'
